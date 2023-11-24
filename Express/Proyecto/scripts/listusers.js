c880547fab6f7eac74084dff662706a8e026471e
Sergio <parrasergio800@gmail.com>
2023-11-23 23:49:44 -0500

    Trabajo en clase

--- Express/Proyecto/scripts/listusers.js
+++ Express/Proyecto/scripts/listusers.js
@@ -2,19 +2,24 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: 'default',
-    host: 'ep-orange-smoke-08960365.us-east-1.postgres.vercel-storage.com',
+    host: "ep-hidden-brook-15386703-pooler.us-east-1.postgres.vercel-storage.com",
     database: 'verceldb',
-    password: 'bf3BTmnKYd4P',
+    password: "dJFbEgO2mf8S",
     port: 5432,
     ssl: {rejectUnauthorized: false}
 });
 
 
-const listUsersQuery = `SELECT * FROM students;`;
+const listUsersQuery = `CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    lastname VARCHAR(50),
+    notes TEXT
+);`;
 
 pool.query(listUsersQuery)
     .then(res => {
-        console.log("List students: ", res.rows);
+        console.log("Tabla creada");
         pool.end();
     })
     .catch(err => {
